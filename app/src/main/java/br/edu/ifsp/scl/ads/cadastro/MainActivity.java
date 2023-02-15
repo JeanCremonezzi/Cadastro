6c609f6cabb90b31beebdafaf8b1795d84e9aebf
JeanCremonezzi <jeancremonezzi@hotmail.com>
2023-02-15 16:15:06 -0300

    feat(MainActivity.java): Save button OnClickListener

--- app/src/main/java/br/edu/ifsp/scl/ads/cadastro/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/ads/cadastro/MainActivity.java
@@ -3,12 +3,25 @@ package br.edu.ifsp.scl.ads.cadastro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button saveBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        saveBtn = (Button) findViewById(R.id.saveBtn);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Hello World!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
